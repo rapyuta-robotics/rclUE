ece08a6ac4bdac63f5732f4e86985c6f95a9c27b
Christian Conti <christian.conti@rapyuta-robotics.com>
2021-02-08 12:12:23 +0900

    Updated ROS2Node

--- Source/rclUE/Private/ROS2Node.cpp
+++ Source/rclUE/Private/ROS2Node.cpp
@@ -2,41 +2,94 @@
 
 
 #include "ROS2Node.h"
+#include "ROS2Subsystem.h"
+#include "Kismet/GameplayStatics.h"
 
 
 // Sets default values
-UROS2Node::UROS2Node()
+AROS2Node::AROS2Node()
 {
 
 }
 
 // Called when the game starts or when spawned
-void UROS2Node::BeginPlay()
+void AROS2Node::BeginPlay()
 {
 	Super::BeginPlay();
-    //RCSOFTCHECK(rclc_node_init_default(&node, "node", "ros_global", &context->Get()));
+
+	context = GetWorld()->GetGameInstance()->GetSubsystem<UROS2Subsystem>()->GetContext();
+    RCSOFTCHECK(rclc_node_init_default(&node, TCHAR_TO_ANSI(*Name), TCHAR_TO_ANSI(*Namespace), &context->Get()));
+
+	executor = NewObject<UROS2Executor>();
+}
+
+void AROS2Node::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	for (auto& s : subs)
+	{
+		RCSOFTCHECK(rcl_subscription_fini(&s.Value, &node));
+	}
+	RCSOFTCHECK(rcl_node_fini(&node));
 }
 
 // Called every frame
-void UROS2Node::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void AROS2Node::Tick(float DeltaTime)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	Super::Tick(DeltaTime);
 
 	// ...
 }
 
-FString UROS2Node::GetName()
+FString AROS2Node::GetName()
 {
-	return NodeName;
+	return Name;
 }
 
-FString UROS2Node::GetNamespace()
+FString AROS2Node::GetNamespace()
 {
-	return NodeNamespace;
+	return Namespace;
 }
 
-UROS2Context* UROS2Node::GetContext()
+void AROS2Node::SetName(FString NodeName)
+{
+	Name = NodeName;
+}
+
+void AROS2Node::SetNamespace(FString NodeNamespace)
+{
+	Namespace = NodeNamespace;
+}
+
+UROS2Context* AROS2Node::GetContext()
 {
 	return context;
 }
 
+rcl_node_t* AROS2Node::GetNode()
+{
+	return &node;
+}
+
+void AROS2Node::Subscribe(FString Topic, FString MsgType)
+{
+	if (!subs.Contains(Topic))
+	{
+		subs.Add(Topic, rcl_get_zero_initialized_subscription());
+		const rosidl_message_type_support_t * type_support = GetTypeSupport(MsgType);
+  		RCSOFTCHECK(rclc_subscription_init_default(&subs[Topic], &node, type_support, TCHAR_TO_ANSI(*Topic)));
+	}
+}
+
+// this should go in a common space as Publisher will need it too
+const rosidl_message_type_support_t* AROS2Node::GetTypeSupport(FString MsgType)
+{
+    // a quick google search suggests that switch-case might not be supported for FString
+    if (MsgType.Equals(TEXT("String")))
+    {
+        return ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String);
+    }
+    else
+    {
+        return nullptr;
+    }
+}
--- Source/rclUE/Public/ROS2Node.h
+++ Source/rclUE/Public/ROS2Node.h
@@ -6,6 +6,7 @@
 #include "Components/ActorComponent.h"
 
 #include "ROS2Context.h"
+#include "ROS2Executor.h"
 
 #include "ROS2Node.generated.h"
 
@@ -14,33 +15,46 @@
  * ROS2 Node that additionally acts as a factory for Publishers, Subscribers, Clients, Services
  */
 UCLASS(Blueprintable)
-class RCLUE_API UROS2Node : public UActorComponent
+class RCLUE_API AROS2Node : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
 	// Sets default values for this actor's properties
-	UROS2Node();
+	AROS2Node();
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
 public:	
 	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	virtual void Tick(float DeltaTime) override;
 
 	
 public:
 	FString GetName();
 	FString GetNamespace();
 
+	rcl_node_t* GetNode();
+
+	void SetName(FString NodeName);
+	void SetNamespace(FString NodeNamespace);
+
+	void Subscribe(FString Topic, FString MsgType); // Topic could be a struct with everything associated with it
+
 private:
 	UROS2Context* GetContext();
+	const rosidl_message_type_support_t* GetTypeSupport(FString MsgType);
 
 	UROS2Context* context;
-	rcl_node_t node;
+	UROS2Executor* executor;
 
-	FString NodeName;
-	FString NodeNamespace;
+	rcl_node_t node;
+	TMap<FString, rcl_subscription_t> subs; // map topic to sub to avoid double subs
+
+	FString Name = TEXT("node");
+	FString Namespace = TEXT("ros_global");
 };
