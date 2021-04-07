a1e521498a2a9d6a21a8ab2a4f30c0d76b245a6e
Christian Conti <christian.conti@rapyuta-robotics.com>
2021-04-07 16:59:22 +0900

    updated rcl(c) stuff to latest versions from git

--- Source/ThirdParty/ros2lib/rcl_interfaces/include/rcl_interfaces/srv/describe_parameters.h
+++ Source/ThirdParty/ros2lib/rcl_interfaces/include/rcl_interfaces/srv/describe_parameters.h
@@ -1,12 +1,1 @@
-// generated from rosidl_generator_c/resource/idl.h.em
-// with input from rcl_interfaces:srv/DescribeParameters.idl
-// generated code does not contain a copyright notice
-
-#ifndef RCL_INTERFACES__SRV__DESCRIBE_PARAMETERS_H_
-#define RCL_INTERFACES__SRV__DESCRIBE_PARAMETERS_H_
-
-#include "rcl_interfaces/srv/detail/describe_parameters__struct.h"
-#include "rcl_interfaces/srv/detail/describe_parameters__functions.h"
-#include "rcl_interfaces/srv/detail/describe_parameters__type_support.h"
-
-#endif  // RCL_INTERFACES__SRV__DESCRIBE_PARAMETERS_H_
+/home/cconti/Rapyuta/ROS2/build/rcl_interfaces/rosidl_generator_c/rcl_interfaces/srv/describe_parameters.h
--- Source/ThirdParty/ros2lib/rcl_interfaces/include/rcl_interfaces/srv/get_parameter_types.hpp
+++ Source/ThirdParty/ros2lib/rcl_interfaces/include/rcl_interfaces/srv/get_parameter_types.hpp
@@ -1,11 +1,1 @@
-// generated from rosidl_generator_cpp/resource/idl.hpp.em
-// generated code does not contain a copyright notice
-
-#ifndef RCL_INTERFACES__SRV__GET_PARAMETER_TYPES_HPP_
-#define RCL_INTERFACES__SRV__GET_PARAMETER_TYPES_HPP_
-
-#include "rcl_interfaces/srv/detail/get_parameter_types__struct.hpp"
-#include "rcl_interfaces/srv/detail/get_parameter_types__builder.hpp"
-#include "rcl_interfaces/srv/detail/get_parameter_types__traits.hpp"
-
-#endif  // RCL_INTERFACES__SRV__GET_PARAMETER_TYPES_HPP_
+/home/cconti/Rapyuta/ROS2/build/rcl_interfaces/rosidl_generator_cpp/rcl_interfaces/srv/get_parameter_types.hpp
