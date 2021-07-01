b5c967c0ce5181ef32876ad1aa461a43a8d0118d
Christian Conti <christian.conti@rapyuta-robotics.com>
2021-07-01 14:10:54 +0900

    updated ue_msgs libraries

--- Source/ThirdParty/ros2lib/ue_msgs/share/ue_msgs/local_setup.sh
+++ Source/ThirdParty/ros2lib/ue_msgs/share/ue_msgs/local_setup.sh
@@ -2,7 +2,7 @@
 
 # since this file is sourced use either the provided AMENT_CURRENT_PREFIX
 # or fall back to the destination set at configure time
-: ${AMENT_CURRENT_PREFIX:="/home/cconti/Rapyuta/turtlebot3_ws/install/ue_msgs"}
+: ${AMENT_CURRENT_PREFIX:="/home/cconti/Rapyuta/colcon_ws/install/ue_msgs"}
 if [ ! -d "$AMENT_CURRENT_PREFIX" ]; then
   if [ -z "$COLCON_CURRENT_PREFIX" ]; then
     echo "The compile time prefix path '$AMENT_CURRENT_PREFIX' doesn't " \
--- Source/ThirdParty/ros2lib/ue_msgs/share/ue_msgs/package.sh
+++ Source/ThirdParty/ros2lib/ue_msgs/share/ue_msgs/package.sh
@@ -52,7 +52,7 @@ _colcon_prepend_unique_value() {
 # since a plain shell script can't determine its own path when being sourced
 # either use the provided COLCON_CURRENT_PREFIX
 # or fall back to the build time prefix (if it exists)
-_colcon_package_sh_COLCON_CURRENT_PREFIX="/home/cconti/Rapyuta/turtlebot3_ws/install/ue_msgs"
+_colcon_package_sh_COLCON_CURRENT_PREFIX="/home/cconti/Rapyuta/colcon_ws/install/ue_msgs"
 if [ -z "$COLCON_CURRENT_PREFIX" ]; then
   if [ ! -d "$_colcon_package_sh_COLCON_CURRENT_PREFIX" ]; then
     echo "The build time path \"$_colcon_package_sh_COLCON_CURRENT_PREFIX\" doesn't exist. Either source a script for a different shell or set the environment variable \"COLCON_CURRENT_PREFIX\" explicitly." 1>&2
