82f739041b3ded602d96436413db0a865d740cf8
Christian Conti <christian.conti@rapyuta-robotics.com>
2021-09-13 08:51:16 +0900

    deleted python related stuff from ThirdParty folder

--- Source/ThirdParty/ros2lib/ue4_interfaces/share/ue4_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(ue4_interfaces_IDL_FILES "srv/AddInts.idl;action/Fibonacci.idl")
-set(ue4_interfaces_INTERFACE_FILES "srv/AddInts.srv;srv/AddInts_Request.msg;srv/AddInts_Response.msg;action/Fibonacci.action")
