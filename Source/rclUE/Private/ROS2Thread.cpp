bcd2cedba40fc8862ac38c0740e2b883e6786e67
Christian Conti <christian.conti@rapyuta-robotics.com>
2021-10-22 11:58:59 +0900

    forgot to uncomment code

--- Source/rclUE/Private/ROS2Thread.cpp
+++ Source/rclUE/Private/ROS2Thread.cpp
@@ -1,41 +1,41 @@
-// // Copyright 2020-2021 Rapyuta Robotics Co., Ltd.
+// Copyright 2020-2021 Rapyuta Robotics Co., Ltd.
 
 
-// #include "ROS2Thread.h"
-// #include "ROS2Node.h"
+#include "ROS2Thread.h"
+#include "ROS2Node.h"
 
 
-// // Sets default values
-// FROS2Thread::FROS2Thread(AROS2Node* Node)
-// {
-// 	if (Node)
-// 	{
-// 		OwnerNode = Node;
-// 	}
-// }
+// Sets default values
+FROS2Thread::FROS2Thread(AROS2Node* Node)
+{
+	if (Node)
+	{
+		OwnerNode = Node;
+	}
+}
 
-// bool FROS2Thread::Init()
-// {
-// 	bStopThread = false;
+bool FROS2Thread::Init()
+{
+	bStopThread = false;
 
-// 	counter = 0;
+	counter = 0;
 
-// 	return true;
-// }
+	return true;
+}
 
-// uint32 FROS2Thread::Run()
-// {
-// 	while (!bStopThread)
-// 	{
-// 		counter++;
+uint32 FROS2Thread::Run()
+{
+	while (!bStopThread)
+	{
+		counter++;
 
-// 		OwnerNode->iterationCounterTest.Enqueue(counter);
-// 	}
+		OwnerNode->iterationCounterTest.Enqueue(counter);
+	}
 
-// 	return 0;
-// }
+	return 0;
+}
 
-// void FROS2Thread::Stop()
-// {
+void FROS2Thread::Stop()
+{
 
-// }
+}
