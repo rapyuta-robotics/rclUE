653805912865cfc05506534a94a336492f2346ce
Christian Conti <christian.conti@rapyuta-robotics.com>
2021-10-22 11:58:04 +0900

    added missing ROS2Thread

---
+++ Source/rclUE/Private/ROS2Thread.cpp
@@ -0,0 +1,41 @@
+// // Copyright 2020-2021 Rapyuta Robotics Co., Ltd.
+
+
+// #include "ROS2Thread.h"
+// #include "ROS2Node.h"
+
+
+// // Sets default values
+// FROS2Thread::FROS2Thread(AROS2Node* Node)
+// {
+// 	if (Node)
+// 	{
+// 		OwnerNode = Node;
+// 	}
+// }
+
+// bool FROS2Thread::Init()
+// {
+// 	bStopThread = false;
+
+// 	counter = 0;
+
+// 	return true;
+// }
+
+// uint32 FROS2Thread::Run()
+// {
+// 	while (!bStopThread)
+// 	{
+// 		counter++;
+
+// 		OwnerNode->iterationCounterTest.Enqueue(counter);
+// 	}
+
+// 	return 0;
+// }
+
+// void FROS2Thread::Stop()
+// {
+
+// }
---
+++ Source/rclUE/Public/ROS2Thread.h
@@ -0,0 +1,32 @@
+// // Copyright 2020-2021 Rapyuta Robotics Co., Ltd.
+
+// #pragma once
+
+// #include "CoreMinimal.h"
+// #include "Core/Public/HAL/Runnable.h"
+
+// class FRunnableThread;
+// class AROS2Node;
+
+// // does this work with dynamic delegates?
+// // based on https://www.youtube.com/watch?v=tGNaDagBq8E
+// class RCLUE_API FROS2Thread : public FRunnable
+// {
+	
+// public:	
+// 	// Sets default values for this actor's properties
+// 	FROS2Thread(AROS2Node* Node);
+
+// 	virtual bool Init();
+
+// 	virtual uint32 Run();
+
+// 	virtual void Stop();
+
+// 	bool bStopThread;
+
+// 	AROS2Node* OwnerNode;
+
+// 	// testing
+// 	int32 counter;
+// };
