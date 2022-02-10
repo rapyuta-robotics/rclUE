0782ab3d6a216747f8e4edd9e7e9619245d9d18e
Russ Webber <russ.webber@greenroomrobotics.com>
2022-02-10 11:38:57 +1100

    fix error reporting to report RCL errmsg, not crash the game

--- Source/rclUE/Public/rclcUtilities.h
+++ Source/rclUE/Public/rclcUtilities.h
@@ -32,13 +32,21 @@ DECLARE_LOG_CATEGORY_EXTERN(LogROS2Action, Log, All);
         rcl_ret_t temp_rc = fn;                                                     \
         if ((temp_rc != RCL_RET_OK))                                                \
         {                                                                           \
-            UE_LOG(LogTemp,                                                         \
-                   Error,                                                           \
-                   TEXT("Failed status on line %d (function %s): %d. Continuing."), \
-                   __LINE__,                                                        \
-                   *FString(__FUNCTION__),                                          \
-                   (int)temp_rc);                                                   \
-            check(temp_rc == 0)                                                     \
+            if (rcutils_error_is_set()) {\
+                UE_LOG(LogTemp,                                                         \
+                    Error,                                                           \
+                    TEXT("RCL error occured on %s:%d: Error code %d. Error msg: %s"),  \
+                    *FString(__FUNCTION__), __LINE__, (int) temp_rc, *FString(rcutils_get_error_string().str)); \
+                rcutils_reset_error(); \
+            } else { \
+                UE_LOG(LogTemp,                                                         \
+                    Error,                                                           \
+                    TEXT("Failed status on line %d (function %s): %d."), \
+                    __LINE__,                                                        \
+                    *FString(__FUNCTION__),                                          \
+                    (int)temp_rc);                                                   \
+            } \
+            ensure(temp_rc == 0);                                                     \
         }                                                                           \
     }
 
