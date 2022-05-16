320fe4afcff47d14e1ffdfc24d82d543609e85bc
viktor <vilkun@n-ix.com>
2022-05-16 17:44:45 +0300

    ros2lib is now 'ros' and fully autogenerated

--- Scripts/build_ros_libs.sh
+++ Scripts/build_ros_libs.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 
 # rclUE scripts generate and rename this folders. Need to cleanup:
-cd $2
-sudo rm -r -f build install log
-sudo rm -r -f build_renamed install_renamed
-cd $3
-sudo rm -r -f build install log
-sudo rm -r -f build_renamed install_renamed
-cd $4
-sudo rm -r -f build install log
-sudo rm -r -f build_renamed install_renamed
+cleanup() {
+    sudo rm -r -f $1/build $1/install $1/log
+    sudo rm -r -f $1/build_renamed $1/install_renamed
+}
+
+cleanup $2
+cleanup $3
+cleanup $4
 
 export ROS_DOMAIN_ID=10
-# pay attention it can be 'rmw_fastrtps_cpp' too
-export RMW_IMPLEMENTATION=rmw_fastrtps_dynamic_cpp
+# pay attention it can be 'rmw_fastrtps_dynamic_cpp' too
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 # Building ros by exact UE clang toolchain
 export MY_SYS_ROOT_PATH=$1"/Engine/Extras/ThirdPartyNotUE/SDKs/HostLinux/Linux_x64/v19_clang-11.0.1-centos7/x86_64-unknown-linux-gnu"
 export CC=$MY_SYS_ROOT_PATH"/bin/clang"
@@ -28,13 +27,15 @@ export MY_LINKER_FLAGS="-latomic "\
 "-Wl,-rpath-link=/usr/lib "
 
 cd $2
-colcon build --cmake-clean-cache --cmake-force-configure --cmake-args "-DCMAKE_SHARED_LINKER_FLAGS='$MY_LINKER_FLAGS'" "-DCMAKE_EXE_LINKER_FLAGS='$MY_LINKER_FLAGS'" "-DBUILD_TESTING=OFF" --no-warn-unused-cli
+colcon build --cmake-clean-cache --cmake-force-configure --cmake-args "-DCMAKE_SHARED_LINKER_FLAGS='$MY_LINKER_FLAGS'" "-DCMAKE_EXE_LINKER_FLAGS='$MY_LINKER_FLAGS'" -DBUILD_TESTING=OFF --no-warn-unused-cli
 
 chmod a+x $2/install/local_setup.bash
 . $2/install/local_setup.bash
 
-cd $3
-colcon build --packages-up-to --allow-overriding rclc --cmake-args "-DCMAKE_SHARED_LINKER_FLAGS='$MY_LINKER_FLAGS'" "-DCMAKE_EXE_LINKER_FLAGS='$MY_LINKER_FLAGS'" "-DBUILD_TESTING=OFF" 
+build_package() {
+    cd $1
+    colcon build --packages-up-to --allow-overriding $2 --cmake-args "-DCMAKE_SHARED_LINKER_FLAGS='$MY_LINKER_FLAGS'" "-DCMAKE_EXE_LINKER_FLAGS='$MY_LINKER_FLAGS'" -DBUILD_TESTING=OFF --no-warn-unused-cli
+}
 
-cd $4
-colcon build --packages-up-to --allow-overriding ue_msgs --cmake-args "-DCMAKE_SHARED_LINKER_FLAGS='$MY_LINKER_FLAGS'" "-DCMAKE_EXE_LINKER_FLAGS='$MY_LINKER_FLAGS'" "-DBUILD_TESTING=OFF" 
+build_package $3 rclc
+build_package $4 ue_msgs
--- Scripts/update_ros.py
+++ Scripts/update_ros.py
@@ -23,41 +23,63 @@ import os, sys, shutil, re, time, subprocess
 from libs_utils import *
 
 # main arguments:
-UE = "/home/vilkun/UE/UnrealEngine" # contains 'Engine' folder
+UE = '/home/vilkun/UE/UnrealEngine' # contains 'Engine' folder
 ros = '/home/vilkun/ros2_foxy' # '/opt/ros/foxy/lib'
-rclc = '/home/vilkun/work/build_foxy/rclc/rclc'
-ue_msgs = '/home/vilkun/work/build_foxy/UE_msgs'
+rclc = '/home/vilkun/work/build_foxy/rclc' # includes rclc_lifecycle
+ue_msgs = '/home/vilkun/work/build_foxy/UE_msgs' # check https://github.com/rapyuta-robotics/UE_msgs
 turtlebot3 = '/home/vilkun/work/turtlebot3-UE'
 allowed_spaces = [ \
-    #ue4_interfaces
-    #ue_msgs
-    "action_msgs", \
-    "builtin_interfaces", \
-    "example_interfaces", \
-    "fastcdr", \
-    "fastrtps", \
-    "geometry_msgs", \
-    "nav_msgs", \
-    "rcl.", \
-    "rcl_", \
-    "rclc.", \
-    "rcpputils", \
-    "rcutils", \
-    "rmw", \
-    "rosgraph_msgs", \
-    "rosidl", \
-    "sensor_msgs", \
-    "std_msgs", \
-    "tf2_msgs", \
-    "tracetools", \
-    "unique_identifier_msgs", \
-    "yaml"]
-not_allowed_spaces = ['python', '.so.', 'connext']
+    'action_msgs', \
+    'builtin_interfaces', \
+    'example_interfaces', \
+    'fastcdr', \
+    'fastrtps', \
+    'geometry_msgs', \
+    'nav_msgs', \
+    'rcl', \
+    'rcutils', \
+    'rcpputils', \
+    'rmw', \
+    'rosgraph_msgs', \
+    'rosidl', \
+    'sensor_msgs', \
+    'std_msgs', \
+    'tf2_msgs', \
+    'tracetools', \
+    'unique_identifier_msgs' ] # 'rcl' will include 'rclc'
+rr_spaces = ['ue_msgs']
+allowed_spaces.extend(rr_spaces)
+not_allowed_spaces = [ \
+    '.so.', \
+    'python', \
+    'rclcpp', 
+    'rclpy', 
+    'rcl_logging', \
+    'rclc_examples', \
+    'rclc_parameter', \
+    'connext', \
+    'cyclonedds', \
+    'rmw_fastrtps_dynamic_cpp', \
+    'rosidl_cmake', \
+    'rosidl_default', \
+    'rosidl_generator_cpp', \
+    'rosidl_generator_dds_idl', \
+    'rosidl_generator_py', \
+    'rosidl_runtime_cpp', \
+    'rosidl_runtime_py', \
+    'rosidl_typesupport_introspection_cpp', \
+    'sensor_msgs_py', \
+    'tf2_sensor_msgs', \
+    'tracetools_', \
+    'Codec_', \
+    'Plugin_', \
+    'RenderSystem_', \
+    '_test_type_support']
 
 rclUE =  turtlebot3 + '/Plugins/rclUE'
 buildRosScript = rclUE + '/Scripts/build_ros_libs.sh'
 rclUEBinaries = rclUE + '/Binaries'
-rclUERos = rclUE + '/Source/ThirdParty/ros2lib'
+rclUERos = rclUE + '/Source/ThirdParty/ros'
 rclUERosInclude = rclUERos + '/include'
 rclUERosLib = rclUERos + '/lib'
 rclUEBuildCS =  rclUE + '/Source/rclUE/rclUE.Build.cs'
@@ -73,19 +95,40 @@ rclcInstall = rclc + '/install'
 ue_msgsBuild = ue_msgs + '/build'
 ue_msgsInstall = ue_msgs + '/install'
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     start = time.time()
     
-    print('Building ros...')
-    os.system('chmod +x ' + buildRosScript)
-    os.system('bash ' + buildRosScript + ' ' + UE + ' ' + ros + ' ' + rclc + ' ' + ue_msgs)
+    # print('Building ros...')
+    # os.system('chmod +x ' + buildRosScript)
+    # os.system('bash ' + buildRosScript + ' ' + UE + ' ' + ros + ' ' + rclc + ' ' + ue_msgs)
+
+    # to be sure that we don't use libraries from this paths:
+    rosBuild = RenameDir(rosBuild, renamedSuffix)
+    rosInstall = RenameDir(rosInstall, renamedSuffix)
+    rclcBuild = RenameDir(rclcBuild, renamedSuffix)
+    rclcInstall = RenameDir(rclcInstall, renamedSuffix)
+    ue_msgsBuild = RenameDir(ue_msgsBuild, renamedSuffix)
+    ue_msgsInstall = RenameDir(ue_msgsInstall, renamedSuffix)
 
     # See more comments inside function description
-    print('\n\n\nGrabbing libs...')
-    Grab(rosBuild, rosInstall, renamedSuffix, rclUERosLib, allowed_spaces)
-    Grab(rclcBuild, rclcInstall, renamedSuffix, rclUERosLib, [])
-    Grab(ue_msgsBuild, ue_msgsInstall, renamedSuffix, rclUERosLib, [])
-    CleanDir(rclUERosLib, not_allowed_spaces)
+    if os.path.isdir(rclUERos):
+        shutil.rmtree(rclUERos)
+        
+    os.makedirs(rclUERosInclude)
+    os.makedirs(rclUERosLib)
+
+    print('Grabbing includes...')
+    GrabIncludes(rosInstall, rclUERosInclude, allowed_spaces)
+    GrabIncludes(rclcInstall, rclUERosInclude, allowed_spaces)
+    GrabIncludes(ue_msgsInstall, rclUERosInclude, allowed_spaces)
+    CleanIncludes(rclUERosInclude, not_allowed_spaces)
+
+    print('Grabbing libs...')
+    GrabLibs(rosBuild, rclUERosLib, allowed_spaces)
+    GrabLibs(rclcBuild, rclUERosLib, allowed_spaces)
+    GrabLibs(ue_msgsBuild, rclUERosLib, allowed_spaces)
+    CleanLibs(rclUERosLib, not_allowed_spaces)
+
     RenameLibsWithVersion(rclUERosLib)
     SetRPATH(rclUERosLib)
     InvalidateBinaries(turtlebot3Binaries, rclUEBinaries, rclUEBuildCS)
