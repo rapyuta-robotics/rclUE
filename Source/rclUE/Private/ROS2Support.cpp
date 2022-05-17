c8ab02f0476a9bfdfb4d99d846e52feb1998bd9d
viktor <vilkun@n-ix.com>
2022-05-17 15:03:38 +0300

    reverted test code

--- Source/rclUE/Private/ROS2Support.cpp
+++ Source/rclUE/Private/ROS2Support.cpp
@@ -10,21 +10,7 @@ rclc_support_t& UROS2Support::Get()
 void UROS2Support::Init()
 {
     allocator = rcl_get_default_allocator();
-    
-    // because of hardcode inside:
-    // ~/ros2_foxy/src/ros2/rcpputils/src/find_library.cpp find_library_path()
-    //string ldLibraryPath = envVars[ldLibraryPathKey] + Path.Combine(ROS2LibPath, "lib") + ":";
-   // Environment.SetEnvironmentVariable(ldLibraryPathKey, ldLibraryPath);
-
-    void* handle = dlopen( "librmw_fastrtps_cpp.so", RTLD_LAZY );
-    void* handle1 = dlopen( "librmw_fastrtps_cpp.so", RTLD_LAZY | RTLD_GLOBAL );
-    void* handle2 = dlopen( "librmw_fastrtps_cpp.so", RTLD_NOW | RTLD_GLOBAL );
-    void* handle3 = FPlatformProcess::GetDllHandle(TEXT("librmw_fastrtps_cpp.so"));
-    UE_LOG(LogTemp, Warning, TEXT("VITYO librmw_fastrtps_cpp.so %s"), handle ? TEXT("not null") : TEXT("null"));
-            
-    rcl_ret_t rc = rclc_support_init(&support, 0, nullptr, &allocator);
-    UE_LOG(LogTemp, Warning, TEXT("VITYO rclc_support_init %d"), rc);
-    RCSOFTCHECK(rc);
+    RCSOFTCHECK(rclc_support_init(&support, 0, nullptr, &allocator));
 }  
 
 void UROS2Support::Fini()
