fbbdfef520d56f75c5a3a1ec2811c7c6460d05db
duc <ducanh.than@rapyuta-robotics.com>
2022-09-09 19:22:32 +0900

    rm localize_lib_symbols.py, moved to UE_Tools

--- Scripts/localize_lib_symbols.py
+++
@@ -1,9 +0,0 @@
-from libs_utils import LocalizeLibSymbols
-import argparse
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-    description="Update shared lib's rpath in symbol resolving, forcing its dependencies to be searched in the local folder only")
-    parser.add_argument("--libs_folder_path", type=str, required=True, help="Full libs folder path")
-    args = parser.parse_args()
-    LocalizeLibSymbols(args.libs_folder_path)
