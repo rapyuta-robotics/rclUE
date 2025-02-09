ce236ae296f903c513ad50ce5c9bb232365dd0ca
Yu Okamoto <yuokamoto1988@gmail.com>
2025-02-10 01:18:22 +0900

    udpate cpp version

--- Source/rclUE/rclUE.Build.cs
+++ Source/rclUE/rclUE.Build.cs
@@ -69,7 +69,7 @@ public class rclUE : ModuleRules
 		}
 
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		CppStandard = CppStandardVersion.Cpp17;
+		CppStandard = CppStandardVersion.Latest;
 		if (Target.Platform == UnrealTargetPlatform.Linux)
 		{
 			// In order to ROS shared libs to be linkable by client UE app & for sake of portability, they need to all co-locate in the same folder
